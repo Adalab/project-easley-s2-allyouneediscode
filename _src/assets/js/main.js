349b67d59fd55f1e86c786a98815e35966002d92
Silvia García Cacho <silvia@garca.info>
2018-11-28 20:28:59 +0100

    Start new interaction

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,9 +2,17 @@
 
 /* campo de escritura */
 
-const fillNameSelector = document.querySelector('#full.name');
+const fillNameSelector = document.querySelector('#full-name');
 const cardNameSelector = document.querySelector('.preview__name');
 
-function putTextOn(event){
-    cardNameSelector.innerHTML = cardNameSelector.innerHTML;
-};
+fillNameSelector.addEventListener('keyup', function(e){
+    const writer = e.currentTarget;
+
+    cardNameSelector.innerHTML = writer.value;
+});
+
+/* desplegable */
+
+const buttonDrop = document.querySelector('.dropdown__design');
+
+buttonDrop.addEventListener('click', ()=> buttonDrop.classList.toggle('.hidden'));
