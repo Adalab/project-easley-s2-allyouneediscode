8f6c946894b0044d388e154b71a7a3e101950ec2
Natalia PN <npelegero@gmail.com>
2018-11-30 11:13:54 +0100

    Add fields in JS but not linked to social icons

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -23,6 +23,72 @@ fillOccupationSelector.addEventListener('keyup', function(e){
 });
 
 
+////////////Here should go imagen de perfil//////////
+
+//////////email field/////////////
+
+const fillEmailSelector = document.querySelector('#email');
+const cardEmailSelector = document.querySelector('.icon__mail');
+
+fillEmailSelector.addEventListener('keyup', function(e){
+    const writer = e.currentTarget;
+
+    // cardEmailSelector.innerHTML = writer.value;   Lo estamos buscando////
+});
+
+
+
+/////////////phone  number field////////
+
+const fillPhoneSelector = document.querySelector('#phone');
+const cardPhoneSelector = document.querySelector('.icon__phone');
+
+fillPhoneSelector.addEventListener('keyup', function(e){
+    const writer = e.currentTarget;
+
+    // cardPhoneSelector.innerHTML = writer.value;   Lo estamos buscando////
+});
+
+
+/////////LinkedIn field////////
+
+const fillLinkedInSelector = document.querySelector('#linkedin');
+const cardLinkedInSelector = document.querySelector('.icon__linkedin');
+
+fillLinkedInSelector.addEventListener('keyup', function(e){
+    const writer = e.currentTarget;
+
+    // cardLinkedInSelector.innerHTML = writer.value;   Lo estamos buscando////
+});
+
+
+//////////Github field//////
+
+
+const fillGithubSelector = document.querySelector('#linkedin');
+const cardGithubSelector = document.querySelector('.icon__github');
+
+fillGithubSelector.addEventListener('keyup', function(e){
+    const writer = e.currentTarget;
+
+    // cardGithubSelector.innerHTML = writer.value;   Lo estamos buscando////
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
