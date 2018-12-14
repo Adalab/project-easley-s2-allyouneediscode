3b5326c7e6bce426082a7667574b1ea4c75b214d
Katherine Briceño <kath.gbl@gmail.com>
2018-12-14 22:35:13 +0100

    create js partials

---
+++ _src/assets/js/_reset.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/* Reset button */
+
+const buttonReset = document.querySelector('.preview__reset');
+const formData = document.querySelector('.main__form');
+
+function resetForm() {
+
+    formData.reset();
+    cardNameSelector.innerHTML = 'Nombre Apellido';
+    cardOccupationSelector.innerHTML = 'Front-end developer';
+    previewImage.style.backgroundImage = `url(https://placehold.it/200x200/ffcc00/0000ff/?text=TEXT)`;
+    fakeCheckUploadImage.style.backgroundImage = ``;
+    cardSelector.innerHTML = '';
+    cardNameSelector.classList.add('preview__name--green');
+    decoRectangleSelector.classList.add('preview__decoration-rectangle--green');
+    for (let i = 0; i < socialIconSelector.length; i++) {
+        socialIconSelector[i].classList.add('social-icon--green');
+    }
+    for (let i = 0; i < skillIconSelector.length; i++) {
+        skillIconSelector[i].classList.add('skill--green');
+    }
+    cardNameSelector.classList.remove('preview__name--red', 'preview__name--grey');
+    decoRectangleSelector.classList.remove('preview__decoration-rectangle--red', 'preview__decoration-rectangle--grey');
+    for (let i = 0; i < socialIconSelector.length; i++) {
+        socialIconSelector[i].classList.remove('social-icon--red', 'social-icon--grey');
+    }
+    for (let i = 0; i < skillIconSelector.length; i++) {
+        skillIconSelector[i].classList.remove('skill--red', 'skill--grey');
+    }
+    cardTextSelector.classList.add('comic-sans');
+    cardTextSelector.classList.remove('ubuntu', 'montserrat');
+    htmlLabel.classList.add('hidden');
+    cssLabel.classList.add('hidden');
+    reactLabel.classList.add('hidden');
+
+}
+
+buttonReset.addEventListener('click', resetForm);
---
+++ _src/assets/js/_typography.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//When 'click'-ing checkbox, add the class corresponding to the selected typography and remove others
+
+function chooseFont() {
+
+    if ((this.value) === 'font-ubuntu') {
+        cardTextSelector.classList.add('ubuntu');
+        cardTextSelector.classList.remove('comic-sans');
+        cardTextSelector.classList.remove('montserrat');
+
+        localStorage.setItem('typography', '1');
+        jsonObject.typography = 1;
+    }
+
+    else if ((this.value) === 'font-comic-sans') {
+        cardTextSelector.classList.add('comic-sans');b
+        cardTextSelector.classList.remove('ubuntu');
+        cardTextSelector.classList.remove('montserrat');
+
+        localStorage.setItem('typography', '2');
+        jsonObject.typography = 2;
+    }
+
+    else if ((this.value) === 'font-montserrat') {
+        cardTextSelector.classList.add('montserrat');
+        cardTextSelector.classList.remove('ubuntu');
+        cardTextSelector.classList.remove('comic-sans');
+
+        localStorage.setItem('typography', '3');
+        jsonObject.typography = 3;
+    }
+}
+
+checkboxUbuntuSelector.addEventListener('click', chooseFont);
+checkboxComicSansSelector.addEventListener('click', chooseFont);
+checkboxMontserratSelector.addEventListener('click', chooseFont);
